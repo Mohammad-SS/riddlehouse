fb416f4828870fa54b912fca8d218bdc8ffdf72e
Mazimi <mohammad.azimi.1996@gmail.com>
2022-11-16 12:59:16 +0330

    testing celery settings

--- qom/riddlehouse/celery.py
+++ qom/riddlehouse/celery.py
@@ -1,7 +1,7 @@
 import os
 
 from celery import Celery
-
+from django.conf import settings
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'riddlehouse.settings')
 
@@ -11,7 +11,7 @@ app = Celery('riddlehouse')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object(settings, namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
