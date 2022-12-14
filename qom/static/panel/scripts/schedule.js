8a9de62b50738e9eb15543bcfa1e690037ca2e0c
Mahdi1419 <mahdi.bagherzade@outlook.com>
2022-12-14 23:47:13 +0330

    fix ui bug

--- qom/static/panel/scripts/schedule.js
+++ qom/static/panel/scripts/schedule.js
@@ -2,58 +2,29 @@ var app = new Vue({
     delimiters: ["[[", "]]"],
     el: '#app',
     data: {
-        add_schedule_pupop: false,
-        add_loschedule_ading: false,
-        tags: [],
-        tag_remove_all: false,
-        time: '00:00',
-        schedule_mode: 1,
-
+        remove_schedule_pupop: false,
+        remove_schedule_loading: false,
+        remove_url: null
     },
 
     methods: {
-        two_digits: (num) => num.toLocaleString('en-US', {
-            minimumIntegerDigits: 2,
-            useGrouping: false
-        }),
-
-        set_time: function (e) {
-            let hour = ''
-            let min = ''
-            let value = e.target.value
-            hour = parseInt(value / 12)
-            min = parseInt((value * 5) - (hour * 60))
-            this.time = this.two_digits(hour) + ":" + this.two_digits(min)
-        },
-
-        addTag() {
-            if (this.time.trim().length === 0 || this.tags.includes(this.time.trim())) {
-                return
-            }
-            this.tags.push(this.time.trim())
-        },
-        removeTag(tag) {
-            this.tags = this.tags.filter(t => t !== tag)
-        },
-
-        
-        reset_time_picker: function (e) {
-            this.$refs.timepicker.value = '0'
-        },
-
-        handle_tag_remove_all: function() {
-            if (this.tag_remove_all) {
-                this.tags = []
-                this.tag_remove_all = false
+    
+        handle_schedule_remove_submit: function(e) {
+            e.preventDefault();
+            let form = this.$refs.schedule_remove_form
+            if (form.checkValidity()) {
+                this.remove_schedule_loading = true;
+                form.submit()
             } else {
-                if (this.tags.length > 0) {
-                    this.tag_remove_all = true
-                }
+                this.remove_schedule_loading = false;
             }
+            
+        },
+
+        handle_remove_schedule: function(url) {
+            this.remove_url = url
+            this.remove_schedule_pupop = true
         }
     },
 });
-
-
-jalaliDatepicker.startWatch();
 
