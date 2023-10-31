1e611b9726845678cd1e861e13d9d3d50723a105
Mahdi1419 <99290119+Mahdi1419@users.noreply.github.com>
2023-10-31 13:21:11 +0330

    update schedule page

--- qom/game/migrations/0003_remove_exclusion_zones_exclusion_from_date_and_more.py
+++ qom/game/migrations/0003_remove_exclusion_zones_exclusion_from_date_and_more.py
@@ -11,10 +11,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name="exclusion",
-            name="zones",
-        ),
+        # migrations.RemoveField(
+        #     model_name="exclusion",
+        #     name="zones",
+        # ),
         migrations.AddField(
             model_name="exclusion",
             name="from_date",
