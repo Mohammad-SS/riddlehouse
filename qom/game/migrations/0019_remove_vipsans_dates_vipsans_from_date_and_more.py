b44ff59f3f26b4fd68b822b3571d12d5bc890fed
Mahdi1419 <99290119+Mahdi1419@users.noreply.github.com>
2023-10-31 12:31:30 +0330

    update schedule page

--- qom/game/migrations/0019_remove_vipsans_dates_vipsans_from_date_and_more.py
+++ qom/game/migrations/0019_remove_vipsans_dates_vipsans_from_date_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('game', '0019_vipsans_onetimevipsans'),
+        ('game', '0018_room_is_archive_vipsans_onetimevipsans'),
     ]
 
     operations = [
--- qom/game/migrations/0020_vipsans_hours.py
+++ qom/game/migrations/0020_vipsans_hours.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('game', '0020_remove_vipsans_dates_vipsans_from_date_and_more'),
+        ('game', '0019_remove_vipsans_dates_vipsans_from_date_and_more'),
     ]
 
     operations = [
