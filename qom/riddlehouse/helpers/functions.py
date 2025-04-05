b8da672a3a979c17432b6dc694fbbf83d1295467
mazimi surface <mohammad.azimi.1996@gmail.com>
2025-04-05 13:06:30 +0330

    functions changed

--- qom/riddlehouse/helpers/functions.py
+++ qom/riddlehouse/helpers/functions.py
@@ -51,59 +51,6 @@ def create_random_authority():
     return authority
 
 
-def start_payment(**kwargs):
-    merchant = get_setting(enums.DefaultSettings.ZARINPAL_MERCHANT_KEY, "cd7b446a-17d3-11e9-ab83-005056a205be")
-    zarinpal_url = get_setting(enums.DefaultSettings.ZARINPAL_START_URL)
-    callback_url = get_setting(enums.DefaultSettings.ZARINPAL_CALLBACK_URL)
-    zarinpal_payment_url = get_setting(enums.DefaultSettings.ZARINPAL_PAYMENT_URL)
-    amount = kwargs.get("amount", None)
-    if not merchant or not amount:
-        return False
-    coupon = kwargs.get("coupon", None)
-
-    room_id = kwargs.get("room_id", None)
-    room = game_models.Room.objects.get(pk=room_id)
-    coupon = orders_models.Coupon.objects.filter(code=coupon)
-    amount = int(amount)
-    if coupon.exists():
-        coupon = coupon[0]
-    else:
-        coupon = None
-
-    if validate_coupon(room, coupon):
-        coupon.used += 1
-        coupon.save()
-        amount = get_amount_after_coupon(coupon, amount)
-
-    kwargs['amount'] = amount
-    if amount == 0:
-        kwargs['authority'] = create_random_authority()
-        payment_obj = create_payment_object(**kwargs)
-        return {"valid": True,
-                "url": callback_url + "?Authority=" + kwargs['authority'] + "&Status=" + "OK"}
-    #         "customer_name": kwargs.get('customer_name', None),
-    #         "customer_mobile": kwargs.get('mobile', None),
-    description = f"پرداخت به نام {kwargs.get('customer_name', '')} با شماره تلفن  {kwargs.get('mobile', '')}"
-    print(description)
-    data = {
-        "merchant_id": merchant,
-        "amount": amount * 10,
-        "callback_url": callback_url,
-        "description": description,
-    }
-    request_obj = requests.post(zarinpal_url, data=data)
-    response = json.loads(request_obj.text)
-    if response['errors']:
-        return {"valid": False, "data": response['errors']}
-    else:
-        kwargs['authority'] = response['data']['authority']
-        payment_obj = create_payment_object(**kwargs)
-        if payment_obj:
-            return {"valid": True, "data": response['data'],
-                    "url": zarinpal_payment_url + response['data']['authority']}
-        else:
-            return {"valid": False, "data": "Failed to create payment object"}
-
 
 def create_payment_object(**kwargs):
     try:
@@ -129,51 +76,6 @@ def create_payment_object(**kwargs):
     obj = orders_models.Payment(**fields)
     obj.save()
     return obj
-
-
-def verify_payment(authority):
-    merchant = get_setting(enums.DefaultSettings.ZARINPAL_MERCHANT_KEY, "cd7b446a-17d3-11e9-ab83-005056a205be")
-    try:
-        payment = orders_models.Payment.objects.get(authority_key=authority)
-    except exceptions.ObjectDoesNotExist as e:
-        print(e)
-        return {"valid": False, "data": "No Authority key found", "payment": None, "order": None}
-    #ordered_before = orders_models.Order.objects.filter(reserved_time=payment.reserved_time, room=payment.room)
-    ordered_before = orders_models.Order.objects.filter(reserved_time=payment.reserved_time, room=payment.room).exclude(customer_number=payment.customer_mobile)
-
-    if ordered_before.exists():
-        print(ordered_before)
-        return {"valid": False, "ordered_before": True, "room": payment.room}
-    amount = payment.amount * 10
-    if amount == 0:
-        order_object, payment_object = place_order(authority, "رایگان - " + authority[0:15], "بدون پرداخت")
-        if not order_object.user_sms_bulk or not order_object.admin_sms_bulk:
-            send_sms.delay(order=order_object.pk)
-        data = {
-            "code": 200,
-            "ref_id": "رایگان",
-            "card_pan": "ندارد",
-            "card_hash": "رایگان",
-        }
-        return {"valid": True, "data": data, "order": order_object, "payment": payment_object}
-    url = get_setting(enums.DefaultSettings.ZARINPAL_VERIFY_URL)
-    data = {
-        "merchant_id": merchant,
-        "amount": amount,
-        "authority": authority
-    }
-    request = requests.post(url, data=data)
-    response = json.loads(request.text)
-    if response['errors']:
-        return {"valid": False, "data": response['errors'], "payment": payment, "order": None}
-    else:
-        order_object, payment_object = place_order(authority, response['data']['ref_id'], response['data']['card_pan'])
-        if order_object:
-            if not order_object.user_sms_bulk or not order_object.admin_sms_bulk:
-                send_sms.delay(order=order_object.pk)
-            return {"valid": True, "data": response['data'], "order": order_object, "payment": payment_object}
-        else:
-            return {"valid": False, "data": "Failed to place order", "payment": None, "order": None}
 
 
 def place_order(authority, transaction_number=None, card_pan=None):
